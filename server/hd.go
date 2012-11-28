367e4fa4a50e7c7169951c17df850a7f53196b77
Chad Clark <chad-git-ice.superfrink.net@superfrink.net>
2012-11-28 12:35:53 -0700

    Fix for a command sent in multiple segments being seen as two invalid commands.  Github issue #2.

--- server/hd.go
+++ server/hd.go
@@ -1,4 +1,5 @@
 package main
+
 // project: distributed hash
 // file: hd.go - a single hash daemon
 // purpose: the daemon for a single node in a distributed hash table
@@ -12,13 +13,13 @@ import "strings"
 
 type HashResponse struct {
 	status string
-	value string
+	value  string
 }
 type HashRequest struct {
-	cmd string
-	key string
+	cmd   string
+	key   string
 	value string
-	out chan HashResponse
+	out   chan HashResponse
 }
 
 var debug bool
@@ -26,30 +27,45 @@ var listenPort uint
 
 func handleClientRequest(conn net.Conn, hashAccessor chan HashRequest) {
 
-	maxBufSize := 1024  // FIXME: is this size good?
+	maxBufSize := 1024 // FIXME: is this size good?
 
-	for {
-		buf := make([]byte, maxBufSize)
+	for { // read and process a command loop
 
-		byteCount, err := conn.Read(buf);
-		if err != nil {
-			log.Println(err)
-			conn.Close();
-			return
-		}
+		requestString := ""
 
-		// FIXME : it would be better to append to a buffer for long requests
-		if byteCount >= maxBufSize {
+		for strings.Index(requestString, "\n") < 0 { // read until we find a single command loop
 
-			byteCount, err = conn.Write([]byte(fmt.Sprintf("Reuqests must be shorter than %d bytes.\n", maxBufSize)));
+			buf := make([]byte, maxBufSize)
+
+			byteCount, err := conn.Read(buf)
 			if err != nil {
 				log.Println(err)
-				conn.Close();
+				conn.Close()
 				return
 			}
-			log.Println("request greater than buffer size")
-			conn.Close();
-			return
+
+			// FIXME : it would be better to append to a buffer for long requests
+			if byteCount >= maxBufSize {
+
+				byteCount, err = conn.Write([]byte(fmt.Sprintf("Reuqests must be shorter than %d bytes.\n", maxBufSize)))
+				if err != nil {
+					log.Println(err)
+					conn.Close()
+					return
+				}
+				log.Println("request greater than buffer size")
+				conn.Close()
+				return
+			}
+
+			// FIXME : We throw away the rest of what we read from the network.
+			// This means that in a command like:
+			//   (echo -ne 'GET a\nGET b\n') | nc -q1 localhost 1742
+			// only the "GET a" is processed but in a command like:
+			//   (echo -ne 'GET a\n' ; sleep 1 ; echo -ne 'GET b\n') | nc -q1 localhost 1742
+			// both "GET a" and "GET b" are processed.
+			bufStr := string(buf) // FIXME : can we index without this temp variable?
+			requestString += string(buf[0 : strings.Index(bufStr, "\n")+1])
 		}
 
 		// // echo back the number of bytes read and the message.
@@ -66,7 +82,6 @@ func handleClientRequest(conn net.Conn, hashAccessor chan HashRequest) {
 		// 	return
 		// }
 
-		requestString := string(buf)
 		requestString = strings.Trim(requestString, string(0)) // trim buffer null bytes
 		requestString = strings.TrimSpace(requestString)       // trim trailing newlines, github issue #1
 
@@ -79,14 +94,14 @@ func handleClientRequest(conn net.Conn, hashAccessor chan HashRequest) {
 			parts := strings.SplitN(requestString, " ", 2)
 			cmd := parts[0]
 			key := parts[1]
-			conn.Write([]byte(fmt.Sprintf("cmd: %s\n", cmd)));
-			conn.Write([]byte(fmt.Sprintf("key: %s\n", key)));
-			conn.Write([]byte(fmt.Sprintf("len: %v\n", len(key))));
+			conn.Write([]byte(fmt.Sprintf("cmd: %s\n", cmd)))
+			conn.Write([]byte(fmt.Sprintf("key: %s\n", key)))
+			conn.Write([]byte(fmt.Sprintf("len: %v\n", len(key))))
 
 			if 0 == len(key) {
-				conn.Write([]byte("Missing key in GET\n"));
+				conn.Write([]byte("Missing key in GET\n"))
 				log.Println("missing key in GET")
-				conn.Close();
+				conn.Close()
 				return
 			}
 
@@ -98,21 +113,21 @@ func handleClientRequest(conn net.Conn, hashAccessor chan HashRequest) {
 			request.out = responseChannel
 			hashAccessor <- request
 
-			response := <- responseChannel
+			response := <-responseChannel
 			fmt.Printf("response: '%+v'\n", response)
 			if "EXISTS" == response.status {
-				conn.Write([]byte(fmt.Sprintf("%s %d %s\n", response.status, len(response.value), response.value)));
+				conn.Write([]byte(fmt.Sprintf("%s %d %s\n", response.status, len(response.value), response.value)))
 			} else {
-				conn.Write([]byte(fmt.Sprintf("%s\n", response.status)));
+				conn.Write([]byte(fmt.Sprintf("%s\n", response.status)))
 			}
 
 		} else if 0 == strings.Index(requestString, "PUT ") {
 			//conn.Write([]byte("put\n"));
 
 			if strings.Count(requestString, " ") < 2 {
-				conn.Write([]byte("Missing value in PUT\n"));
+				conn.Write([]byte("Missing value in PUT\n"))
 				log.Println("missing value in PUT")
-				conn.Close();
+				conn.Close()
 				return
 			}
 
@@ -120,9 +135,9 @@ func handleClientRequest(conn net.Conn, hashAccessor chan HashRequest) {
 			cmd := parts[0]
 			key := parts[1]
 			val := parts[2]
-			conn.Write([]byte(fmt.Sprintf("cmd: %s\n", cmd)));
-			conn.Write([]byte(fmt.Sprintf("key: %s\n", key)));
-			conn.Write([]byte(fmt.Sprintf("val: %s\n", val)));
+			conn.Write([]byte(fmt.Sprintf("cmd: %s\n", cmd)))
+			conn.Write([]byte(fmt.Sprintf("key: %s\n", key)))
+			conn.Write([]byte(fmt.Sprintf("val: %s\n", val)))
 
 			responseChannel := make(chan HashResponse)
 
@@ -133,7 +148,7 @@ func handleClientRequest(conn net.Conn, hashAccessor chan HashRequest) {
 			request.out = responseChannel
 			hashAccessor <- request
 
-			response := <- responseChannel
+			response := <-responseChannel
 			fmt.Printf("response: '%+v'\n", response)
 
 			// FIXME : incomplete
@@ -147,35 +162,35 @@ func createHashAccessor(table map[string]string) chan HashRequest {
 
 	go func() {
 		for {
-			request := <- requestChannel
+			request := <-requestChannel
 			fmt.Printf("hashAccessor - cmd %s\n", request.cmd)
-				switch request.cmd {
-				case "GET":
-					var response HashResponse
-					var ok bool
+			switch request.cmd {
+			case "GET":
+				var response HashResponse
+				var ok bool
 
-					if response.value, ok = table[request.key]; ok {
-						response.status = "EXISTS"
-					} else {
-						response.status = "NOTEXISTS"
-					}
-
-					go func() { request.out <- response }()
-
-				case "PUT":
-					table[request.key] = request.value
-					var response HashResponse
-					response.status = "OK"
-					go func() { request.out <- response }()
-
-				default:
-					fmt.Printf("hashAccessor - unexpected cmd %s\n", request.cmd)
-					// FIXME : incomplete
+				if response.value, ok = table[request.key]; ok {
+					response.status = "EXISTS"
+				} else {
+					response.status = "NOTEXISTS"
 				}
+
+				go func() { request.out <- response }()
+
+			case "PUT":
+				table[request.key] = request.value
+				var response HashResponse
+				response.status = "OK"
+				go func() { request.out <- response }()
+
+			default:
+				fmt.Printf("hashAccessor - unexpected cmd %s\n", request.cmd)
+				// FIXME : incomplete
+			}
 		}
 	}()
 
-	return requestChannel;
+	return requestChannel
 }
 
 func main() {
@@ -186,8 +201,8 @@ func main() {
 	flag.UintVar(&listenPort, "p", 1742, "port to listen on")
 	flag.Parse()
 
-	fmt.Printf("debug: '%v'\n", debug);
-	fmt.Printf("Listen port: '%d'\n", listenPort);
+	fmt.Printf("debug: '%v'\n", debug)
+	fmt.Printf("Listen port: '%d'\n", listenPort)
 
 	// GOAL : create the storage for the hash table
 
