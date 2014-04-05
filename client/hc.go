8db2b65b94cac74135dd5316a7c8da66bcd610c9
Chad Clark <chad-git-ice.superfrink.net@superfrink.net>
2014-04-05 15:46:43 -0600

    - Add code to read a config file pointing to hash servers.

--- client/hc.go
+++ client/hc.go
@@ -6,26 +6,58 @@ package main
 // git: FIXME
 
 // import "bytes"
+import "encoding/json"
 import "flag"
 import "fmt"
+import "io/ioutil"
 // import "net"
 import "os"
-// import "strings"
+import "strings"
 
+type HashServerConfig struct {
+	Servers []string
+}
 
 type HashResponse struct {
-	status string
-	value  string
+	Status string
+	Value  string
 }
 
 type HashCommand struct {
-	cmd   string
-	key   string
-	value string
-	out   chan HashResponse
+	Cmd   string
+	Key   string
+	Value string
+	Out   chan HashResponse
 }
 
 var debug bool
+
+func read_hash_config(out *HashServerConfig) error {
+
+	config_string, err := ioutil.ReadFile("hc.conf")
+	if nil != err {
+		return err
+	}
+
+	err = json.Unmarshal(config_string, out)
+	if nil != err {
+		return err
+	}
+
+	return nil
+}
+
+func select_hash_server() {
+	// FIXME : incomplete
+}
+
+func hash_read() {
+	// FIXME : incomplete
+}
+
+func hash_write() {
+	// FIXME : incomplete
+}
 
 func main() {
 
@@ -41,13 +73,34 @@ func main() {
 	   os.Exit(1)
 	}
 
+	// GOAL : read the command from the user
+
 	var cliRequest HashCommand
-	cliRequest.cmd = flag.Arg(0)
-	cliRequest.key = flag.Arg(1)
+	cliRequest.Cmd = flag.Arg(0)
+	cliRequest.Key = flag.Arg(1)
 
 	// FIXME : read value if NArg == 3
 
-	fmt.Printf("%+v\n", cliRequest)
+	cliRequest.Cmd = strings.ToUpper(cliRequest.Cmd)
 
-	// FIXME : incomplete
+	fmt.Printf("cliRequest %+v\n", cliRequest)
+
+	// GOAL : read the hash server config
+
+	var hash_config HashServerConfig
+	err := read_hash_config(&hash_config)
+	if nil != err {
+		fmt.Printf("error unable to read config %v\n", err);
+	}
+	fmt.Printf("hash_config: %+v\n", hash_config);
+
+	// GOAL : execute the command from the user
+
+	if "GET" == cliRequest.Cmd {
+		
+		// FIXME : incomplete; call hash_read()
+	} else if "PUT" == cliRequest.Cmd {
+
+		// FIXME : incomplete; call hash_write()
+	}
 }
